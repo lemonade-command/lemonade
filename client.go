f2cfc631d2e1766f221606fde9d46a06acf70ac1
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-08-22 22:39:15 +0900

    Translate local file to HTTP

--- client.go
+++ client.go
@@ -2,15 +2,64 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
 	"net/rpc"
+	"os"
 )
 
 var dummy = &struct{}{}
 
+func exists(fname string) bool {
+	_, err := os.Stat(fname)
+	return err == nil
+}
+
+func serveFile(fname string) (string, <-chan struct{}, error) {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		return "", nil, err
+	}
+	finished := make(chan struct{})
+
+	go func() {
+		http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			b, err := ioutil.ReadFile(fname)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Write(b)
+
+			w.(http.Flusher).Flush()
+			finished <- struct{}{}
+		}))
+	}()
+
+	return fmt.Sprintf("http://127.0.0.1:%d/%s", l.Addr().(*net.TCPAddr).Port, fname), finished, nil
+}
+
 func (c *CLI) Open() int {
-	return c.withRPCClient(func(client *rpc.Client) error {
-		return client.Call("URI.Open", c.DataSource, dummy)
+	var finished <-chan struct{}
+	st := c.withRPCClient(func(client *rpc.Client) error {
+		var uri string
+		if exists(c.DataSource) {
+			var err error
+			uri, finished, err = serveFile(c.DataSource)
+			if err != nil {
+				return err
+			}
+		} else {
+			uri = c.DataSource
+		}
+
+		return client.Call("URI.Open", uri, dummy)
 	})
+	if finished != nil {
+		<-finished
+	}
+	return st
 }
 
 func (c *CLI) Paste() int {
