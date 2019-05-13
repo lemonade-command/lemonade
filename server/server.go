cee32d58855d3db03bda0f8d68402bf549699aad
涵曦 <hanxi.info@gmail.com>
2019-05-13 22:48:00 +0800

    server use http rest api

--- server/server.go
+++ server/server.go
@@ -2,76 +2,64 @@ package server
 
 import (
 	"fmt"
-	"net"
-	"net/rpc"
+	"io"
+	"io/ioutil"
+	"net/http"
 
+	"github.com/atotto/clipboard"
 	log "github.com/inconshreveable/log15"
-
 	"github.com/lemonade-command/lemonade/lemon"
-	"github.com/pocke/go-iprange"
+	"github.com/skratchdot/open-golang/open"
 )
 
-var connCh = make(chan net.Conn, 1)
+var lineEndingOpt string
 
-var LineEndingOpt string
+//var ra *iprange.Range
+
+func handle_copy(w http.ResponseWriter, req *http.Request) {
+	/*remoteIP, _, err := net.SplitHostPort(req.RemoteAddr)
+	if !ra.IncludeStr(remoteIP) {
+		return
+	}*/
+
+	// Read body
+	b, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	clipboard.WriteAll(lemon.ConvertLineEnding(string(b), lineEndingOpt))
+}
+
+func handle_paste(w http.ResponseWriter, req *http.Request) {
+	t, err := clipboard.ReadAll()
+	if err == nil {
+		io.WriteString(w, t)
+	}
+}
+
+func handle_open(w http.ResponseWriter, req *http.Request) {
+	q := req.URL.Query()
+	url := q.Get("url")
+	open.Run(url)
+}
 
 func Serve(c *lemon.CLI, logger log.Logger) error {
 	port := c.Port
-	allowIP := c.Allow
-	LineEndingOpt = c.LineEnding
-	ra, err := iprange.New(allowIP)
+	//allowIP := c.Allow
+	lineEndingOpt = c.LineEnding
+	/*ra, err = iprange.New(allowIP)
+	if err != nil {
+		return err
+	}*/
+
+	http.HandleFunc("/copy", handle_copy)
+	http.HandleFunc("/paste", handle_paste)
+	http.HandleFunc("/open", handle_open)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		return err
 	}
-
-	addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf(":%d", port))
-	if err != nil {
-		return err
-	}
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		return err
-	}
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			logger.Error(err.Error())
-			continue
-		}
-		logger.Info("Request from " + conn.RemoteAddr().String())
-		if !ra.InlucdeConn(conn) {
-			continue
-		}
-		connCh <- conn
-		rpc.ServeConn(conn)
-	}
-}
-
-// ServeLocal is for fall back when lemonade client can't connect to server.
-// returns port number, error
-func ServeLocal(logger log.Logger) (int, error) {
-	l, err := net.Listen("tcp", "localhost:0")
-	if err != nil {
-		return 0, err
-	}
-	go func() {
-		for {
-			conn, err := l.Accept()
-			if err != nil {
-				logger.Crit(err.Error())
-				continue
-			}
-			connCh <- conn
-			rpc.ServeConn(conn)
-		}
-	}()
-	return l.Addr().(*net.TCPAddr).Port, nil
-}
-
-func init() {
-	uri := &URI{}
-	rpc.Register(uri)
-	clipboard := &Clipboard{}
-	rpc.Register(clipboard)
+	return nil
 }
